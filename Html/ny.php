3d35a2449690d3f821a90662146da4091e079e90
Sopoo <kriswinge@gmail.com>
2015-08-27 21:07:48 +0200

    Flyttet ny.php inn i html mappen

---
+++ Html/ny.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Untitled Document</title>
+</head>
+<body>
+    <h1>Din film/serie har blitt lagt til!</h1>
+    <?php
+        $tittel = $POST["Tittel"];
+        $beskrivelse = $POST["Beskrivelse"];
+        $regi = $POST["Regi"];
+    ?>
+</body>
+</html>
